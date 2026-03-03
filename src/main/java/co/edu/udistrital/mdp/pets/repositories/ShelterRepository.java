4a1e7d8b19588d47cac31c8d6452d755aa6714a5
iyeremy <yeremyvegap@gmail.com>
2026-03-03 07:24:38 -0500

    refactor(shelter): Convert method comments from /** */ to //

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
@@ -8,23 +8,16 @@ import java.util.List;
 @Repository
 public interface ShelterRepository extends JpaRepository<ShelterEntity, Long> {
 
-    /**
-     * Busca refugios por ciudad (Method Named Query)
-     */
+
+    // Busca refugios por ciudad (Method Named Query)
     List<ShelterEntity> findByCity(String city);
 
-    /**
-     * Busca un refugio por su nombre exacto
-     */
+    // Busca un refugio por su nombre exacto
     ShelterEntity findByName(String name);
 
-	/**
-	 *Busca refugio por su nombre parcial
-	 */
+	// Busca refugio por su nombre parcial
 	List<ShelterEntity> findByNameContainingIgnoreCase(String name);
 
-	/**
-	 *Verificar si el correo ya existe
-	 */
+	// Verificar si el correo ya existe
 	Boolean existsByEmailShelter(String emailShelter);
 }
