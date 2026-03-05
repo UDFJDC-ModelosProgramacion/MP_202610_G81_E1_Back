5492bc71383bd20fa7894ff517aa4592493e34c3
Juan Rincon <jprinconr@udistrital.edu.co>
2026-03-04 21:23:21 -0500

    feat(adopter): implement AdopterEntity and AdopterRepository

--- src/main/java/co/edu/udistrital/mdp/pets/entities/AdopterEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/AdopterEntity.java
@@ -1,13 +1,21 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import java.util.List;
 
 @Data
 @Entity
-public class AdopterEntity extends BaseEntity {
+public class AdopterEntity extends UserEntity {
 
     private String housingType;
     private Boolean hasChildren;
     private Boolean hasOtherPets;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "adopter")
+    private List<AdoptionEntity> adoptions;
 }
