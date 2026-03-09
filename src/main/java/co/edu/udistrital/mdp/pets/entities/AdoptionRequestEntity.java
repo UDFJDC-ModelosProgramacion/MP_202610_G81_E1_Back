24fd59276694d99a681c3192ea879381ab85f6d3
iyeremy <yeremyvegap@gmail.com>
2026-03-09 07:55:46 -0500

    feat: update AdoptionRequestEntity with adopter relationship and approval aggregation

--- src/main/java/co/edu/udistrital/mdp/pets/entities/AdoptionRequestEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/AdoptionRequestEntity.java
@@ -1,18 +1,22 @@
 package co.edu.udistrital.mdp.pets.entities;
 
-import java.time.LocalDate;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrePersist;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+import lombok.ToString;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Entity
+/**
+ * Entity representing an Adoption Request.
+ * Integrates with ApprovalStrategy for evaluation and links to TrialCohabitation.
+ */
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
@@ -21,14 +25,26 @@ public class AdoptionRequestEntity extends BaseEntity {
     private LocalDate requestDate;
     private String status;
 
+    // Relation: 1 Adopter has many AdoptionRequests
+    @PodamExclude
+    @ManyToOne
+    @JoinColumn(name = "adopter_id")
+    private AdopterEntity adopter;
+
+    // Relation: 1 Pet has many AdoptionRequests
+    @PodamExclude
     @ManyToOne
     @JoinColumn(name = "pet_id")
     private PetEntity pet;
 
-    @OneToOne
+    // Relation: 1 AdoptionRequest can have 0 or 1 TrialCohabitation
+    @PodamExclude
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "trial_cohabitation_id")
     private TrialCohabitationEntity trialCohabitation;
 
+    // Relation: Aggregation with 1 ApprovalStrategy
+    @PodamExclude
     @ManyToOne
     @JoinColumn(name = "strategy_id")
     private ApprovalStrategyEntity approvalStrategy;
@@ -44,6 +60,10 @@ public class AdoptionRequestEntity extends BaseEntity {
         this.approvalStrategy = approvalStrategy;
     }
 
+    /**
+     * Delegates evaluation logic to the assigned strategy.
+     * @return boolean evaluation result.
+     */
     public boolean evaluate() {
         if (approvalStrategy != null) {
             return approvalStrategy.evaluate(this);
