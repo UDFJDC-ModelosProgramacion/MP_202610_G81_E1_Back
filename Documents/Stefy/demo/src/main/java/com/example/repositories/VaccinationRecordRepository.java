41e479e954bcc95f0bfc1be5b657c2d9d0f12b2c
iyeremy <yeremyvegap@gmail.com>
2026-03-08 23:53:15 -0500

    fix: remove personal directory

--- Documents/Stefy/demo/src/main/java/com/example/repositories/VaccinationRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories; 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.VaccinationRecordEntity;
-
-@Repository
-public interface VaccinationRecordRepository extends JpaRepository<VaccinationRecordEntity, Long> {
-}
