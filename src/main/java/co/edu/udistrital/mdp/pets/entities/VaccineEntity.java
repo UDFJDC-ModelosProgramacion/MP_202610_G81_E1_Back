7a3b6b1a7f63acf1e434901c8570ed8544f4cf9a
iyeremy <yeremyvegap@gmail.com>
2026-03-09 00:11:48 -0500

    fix: import data structure packages

--- src/main/java/co/edu/udistrital/mdp/pets/entities/VaccineEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/VaccineEntity.java
@@ -1,10 +1,14 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import uk.co.jemos.podam.common.PodamExclude;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Data
