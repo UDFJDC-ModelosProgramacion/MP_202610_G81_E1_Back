b3823263bfec871758efad330d5627a711b2c597
iyeremy <yeremyvegap@gmail.com>
2026-03-14 20:49:48 -0500

    feat: add centralized exceptions NOT_FOUND

---
+++ src/main/java/co/edu/udistrital/mdp/pets/exceptions/ErrorMessage.java
@@ -0,0 +1,14 @@
+package co.edu.udistrital.mdp.pets.exceptions;
+
+/**
+ * Utility class to centralize error messages used in services.
+ */
+public final class ErrorMessage {
+    public static final String SHELTER_NOT_FOUND = "The shelter with the given id was not found";
+    public static final String VETERINARIAN_NOT_FOUND = "The veterinarian with the given id was not found";
+    public static final String PET_NOT_FOUND = "The pet with the given id was not found";
+    
+    private ErrorMessage() {
+        throw new IllegalStateException("Utility class");
+    }
+}
