9ef0bc9e2ca8a5da78111c0733e7b077ca67c936
iyeremy <yeremyvegap@gmail.com>
2026-03-14 23:34:05 -0500

    feat(exception): add USER_NOT_FOUNDin ErrorMessage

--- src/main/java/co/edu/udistrital/mdp/pets/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/udistrital/mdp/pets/exceptions/ErrorMessage.java
@@ -7,7 +7,8 @@ public final class ErrorMessage {
     public static final String SHELTER_NOT_FOUND = "The shelter with the given id was not found";
     public static final String VETERINARIAN_NOT_FOUND = "The veterinarian with the given id was not found";
     public static final String PET_NOT_FOUND = "The pet with the given id was not found";
-    
+    public static final String USER_NOT_FOUND = "The user with the given id was not found";
+
     private ErrorMessage() {
         throw new IllegalStateException("Utility class");
     }
