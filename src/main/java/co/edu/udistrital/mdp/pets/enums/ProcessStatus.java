a1238f1c4e0389109ea8425d39b680aa8056c752
iyeremy <yeremyvegap@gmail.com>
2026-03-15 01:38:56 -0500

    chore: add enum for status attribute

---
+++ src/main/java/co/edu/udistrital/mdp/pets/enums/ProcessStatus.java
@@ -0,0 +1,9 @@
+package co.edu.udistrital.mdp.pets.enums;
+
+public enum ProcessStatus {
+    CREATED,      // Creado / Solicitado
+    IN_PROGRESS,  // En curso / En pruebas
+    COMPLETED,    // Finalizado exitosamente
+    CANCELLED,    // Cancelado o Rechazado
+    PENDING       // Pendiente de pago o cita
+}
