886af8fb4cb95cb46b53ec6d388a4b4bb53cad1a
iyeremy <yeremyvegap@gmail.com>
2026-03-21 18:56:50 -0500

    fix: add good implemetation for Strategy Notification

--- src/main/java/co/edu/udistrital/mdp/pets/entities/NotificationStrategyEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/NotificationStrategyEntity.java
@@ -1,9 +1,14 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
 @Entity
+@EqualsAndHashCode(callSuper = true)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "notification_type")
 public abstract class NotificationStrategyEntity extends BaseEntity {
     public abstract void send(NotificationEntity notification);
 }
