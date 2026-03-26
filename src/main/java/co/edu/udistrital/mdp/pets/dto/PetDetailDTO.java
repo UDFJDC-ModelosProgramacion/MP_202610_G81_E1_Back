0cffe0632741607d99372c77233dbe902cd9fa7c
iyeremy <yeremyvegap@gmail.com>
2026-03-26 00:44:40 -0500

    refacture: inicialize List

--- src/main/java/co/edu/udistrital/mdp/pets/dto/PetDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/pets/dto/PetDetailDTO.java
@@ -4,7 +4,7 @@ import co.edu.udistrital.mdp.pets.enums.PetStatus;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.util.List;
-
+import java.util.ArrayList;
 /**
  * Detailed DTO for Pet including all attributes and relationships.
  * Used for detailed view and creation/update processes.
@@ -13,9 +13,9 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 public class PetDetailDTO extends PetDTO {
 
-    private List<ReviewDTO> reviews;
-    private List<AdoptionDTO> adoptions;
-    private List<AdoptionRequestDTO> adoptionRequests;
-    private List<AdoptionFollowUpDTO> followUps;
-    private List<TrialCohabitationDTO> trials;
+    private List<ReviewDTO> reviews = new ArrayList<>();
+    private List<AdoptionDTO> adoptions = new ArrayList<>();
+    private List<AdoptionRequestDTO> adoptionRequests = new ArrayList<>();
+    private List<AdoptionFollowUpDTO> followUps = new ArrayList<>();
+    private List<TrialCohabitationDTO> trials = new ArrayList<>();
 }
