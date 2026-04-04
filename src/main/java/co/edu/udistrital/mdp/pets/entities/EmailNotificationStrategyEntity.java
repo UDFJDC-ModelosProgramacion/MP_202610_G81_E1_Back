ae90a530ee2f51a25ce50e17b32a646f0b9c7730
iyeremy <yeremyvegap@gmail.com>
2026-04-04 15:27:20 -0500

    feat: add empty constructor

--- src/main/java/co/edu/udistrital/mdp/pets/entities/EmailNotificationStrategyEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/EmailNotificationStrategyEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
 @DiscriminatorValue("EMAIL")
+@NoArgsConstructor
 public class EmailNotificationStrategyEntity extends NotificationStrategyEntity {
     @Override
     public void send(NotificationEntity notification) {
--- src/main/java/co/edu/udistrital/mdp/pets/entities/InAppNotificationStrategyEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/InAppNotificationStrategyEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
 @DiscriminatorValue("IN_APP")
+@NoArgsConstructor
 public class InAppNotificationStrategyEntity extends NotificationStrategyEntity {
     @Override
     public void send(NotificationEntity notification) {
