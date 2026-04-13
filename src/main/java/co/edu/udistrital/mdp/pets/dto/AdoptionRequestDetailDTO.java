1725b295795073a0d571962e6e08748e62ffb4b4
iyeremy <yeremyvegap@gmail.com>
2026-04-12 21:43:37 -0500

    fix(request): solve override the equals method in DetailDTO

--- src/main/java/co/edu/udistrital/mdp/pets/dto/AdoptionRequestDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/pets/dto/AdoptionRequestDetailDTO.java
@@ -2,8 +2,10 @@ package co.edu.udistrital.mdp.pets.dto;
 
 import lombok.NoArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class AdoptionRequestDetailDTO extends AdoptionRequestDTO {
     
