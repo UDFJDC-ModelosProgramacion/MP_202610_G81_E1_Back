bf62baad35842339b9a7eb296af81ff125106bae
iYeremy <yeremyvegap@gmail.com>
2026-05-03 20:17:06 -0500

    refactor(pets): remove controller interaction directly with entities

--- src/main/java/co/edu/udistrital/mdp/pets/controllers/PetController.java
+++ src/main/java/co/edu/udistrital/mdp/pets/controllers/PetController.java
@@ -17,6 +17,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,85 +33,88 @@ public class PetController {
     private ModelMapper modelMapper;
 
     @GetMapping
-	@ResponseStatus(code = HttpStatus.OK)
-	public List<PetDTO> findAll(
-        @RequestParam(required = false) String species,
-        @RequestParam(required = false) String size,
-        @RequestParam(required = false) PetStatus status) {
-    
-    List<PetEntity> pets = petService.getPets(species, size, status);
-    return modelMapper.map(pets, new TypeToken<List<PetDTO>>() {}.getType());
-	}
+    public ResponseEntity<List<PetDTO>> findAll(
+            @RequestParam(required = false) String species,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) PetStatus status) {
+        
+        List<PetEntity> pets = petService.getPetsEntities(species, size, status);
+        return ResponseEntity.ok(modelMapper.map(
+            pets, 
+            new TypeToken<List<PetDTO>>() {}.getType()
+        ));
+    }
 
-    @GetMapping(value = "/{id}")
-    @ResponseStatus(code = HttpStatus.OK)
-    public PetDetailDTO findOne(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity entity = petService.getPet(id);
-        return modelMapper.map(entity, PetDetailDTO.class);
+    @GetMapping("/{id}")
+    public ResponseEntity<PetDetailDTO> findOne(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity entity = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(entity, PetDetailDTO.class));
     }
 
     @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public PetDTO create(@RequestBody PetDTO petDTO) throws IllegalOperationException {
-        PetEntity entity = modelMapper.map(petDTO, PetEntity.class);
-        PetEntity newEntity = petService.createPet(entity);
-        return modelMapper.map(newEntity, PetDTO.class);
+    public ResponseEntity<PetDTO> create(@RequestBody PetDTO petDTO) throws IllegalOperationException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(petService.createFromDTO(petDTO));
     }
 
-    @PutMapping(value = "/{id}")
-    @ResponseStatus(code = HttpStatus.OK)
-    public PetDTO update(@PathVariable Long id, @RequestBody PetDTO petDTO) 
+    @PutMapping("/{id}")
+    public ResponseEntity<PetDTO> update(@PathVariable Long id, @RequestBody PetDTO petDTO) 
             throws EntityNotFoundException, IllegalOperationException {
-        PetEntity entity = modelMapper.map(petDTO, PetEntity.class);
-        PetEntity updatedEntity = petService.updatePet(id, entity);
-        return modelMapper.map(updatedEntity, PetDTO.class);
+        return ResponseEntity.ok(petService.updateFromDTO(id, petDTO));
     }
 
-    @DeleteMapping(value = "/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) throws EntityNotFoundException, IllegalOperationException {
         petService.deletePet(id);
     }
 
-    @PostMapping(value = "/{id}/returns")
-    @ResponseStatus(code = HttpStatus.OK)
-    public PetDTO processReturn(@PathVariable Long id) throws EntityNotFoundException, IllegalOperationException {
-        PetEntity returnedPet = petService.processReturn(id);
-        return modelMapper.map(returnedPet, PetDTO.class);
-    }
+    @PostMapping("/{id}/returns")
+	public ResponseEntity<PetDTO> processReturn(@PathVariable Long id) throws EntityNotFoundException, IllegalOperationException {
+		return ResponseEntity.ok(petService.processReturnDTO(id));
+	}
 
     @GetMapping("/{id}/adoptions")
-    @ResponseStatus(code = HttpStatus.OK)
-    public List<AdoptionDTO> getAdoptions(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity pet = petService.getPet(id);
-        return modelMapper.map(pet.getAdoptions(), new TypeToken<List<AdoptionDTO>>() {}.getType());
+    public ResponseEntity<List<AdoptionDTO>> getAdoptions(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity pet = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(
+            pet.getAdoptions(), 
+            new TypeToken<List<AdoptionDTO>>() {}.getType()
+        ));
     }
 
     @GetMapping("/{id}/requests")
-    @ResponseStatus(code = HttpStatus.OK)
-    public List<AdoptionRequestDTO> getRequests(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity pet = petService.getPet(id);
-        return modelMapper.map(pet.getAdoptionRequests(), new TypeToken<List<AdoptionRequestDTO>>() {}.getType());
+    public ResponseEntity<List<AdoptionRequestDTO>> getRequests(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity pet = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(
+            pet.getAdoptionRequests(), 
+            new TypeToken<List<AdoptionRequestDTO>>() {}.getType()
+        ));
     }
 
     @GetMapping("/{id}/follow-ups")
-    @ResponseStatus(code = HttpStatus.OK)
-    public List<AdoptionFollowUpDTO> getFollowUps(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity pet = petService.getPet(id);
-        return modelMapper.map(pet.getFollowUps(), new TypeToken<List<AdoptionFollowUpDTO>>() {}.getType());
+    public ResponseEntity<List<AdoptionFollowUpDTO>> getFollowUps(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity pet = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(
+            pet.getFollowUps(), 
+            new TypeToken<List<AdoptionFollowUpDTO>>() {}.getType()
+        ));
     }
 
     @GetMapping("/{id}/reviews")
-    @ResponseStatus(code = HttpStatus.OK)
-    public List<ReviewDTO> getReviews(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity pet = petService.getPet(id);
-        return modelMapper.map(pet.getReviews(), new TypeToken<List<ReviewDTO>>() {}.getType());
+    public ResponseEntity<List<ReviewDTO>> getReviews(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity pet = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(
+            pet.getReviews(), 
+            new TypeToken<List<ReviewDTO>>() {}.getType()
+        ));
     }
 
     @GetMapping("/{id}/trials")
-    @ResponseStatus(code = HttpStatus.OK)
-    public List<TrialCohabitationDTO> getTrials(@PathVariable Long id) throws EntityNotFoundException {
-        PetEntity pet = petService.getPet(id);
-        return modelMapper.map(pet.getTrials(), new TypeToken<List<TrialCohabitationDTO>>() {}.getType());
+    public ResponseEntity<List<TrialCohabitationDTO>> getTrials(@PathVariable Long id) throws EntityNotFoundException {
+        PetEntity pet = petService.getPetEntity(id);
+        return ResponseEntity.ok(modelMapper.map(
+            pet.getTrials(), 
+            new TypeToken<List<TrialCohabitationDTO>>() {}.getType()
+        ));
     }
 }
